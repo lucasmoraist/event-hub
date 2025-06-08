db715497e005386c7979612d984f082610709805
lucasmoraist <luksmnt1101@gmail.com>
2025-06-08 18:17:18 -0300

    feat: add Swagger configuration and dependencies for API documentation

--- event-hub/src/main/java/com/lucasmoraist/event_hub/domain/request/EventsRequest.java
+++ event-hub/src/main/java/com/lucasmoraist/event_hub/domain/request/EventsRequest.java
@@ -1,5 +1,6 @@
 package com.lucasmoraist.event_hub.domain.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,6 +8,21 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Schema(
+        description = "Request object for creating or updating an event",
+        requiredProperties = {"title", "description", "date", "time", "location", "capacity", "createdBy"},
+        example = """
+                {
+                    "title": "Tech Conference 2023",
+                    "description": "A conference about the latest in technology.",
+                    "date": "2023-10-15",
+                    "time": "09:00",
+                    "location": "Convention Center, Cityville",
+                    "capacity": 500,
+                    "createdBy": "admin"
+                }
+                """
+)
 public record EventsRequest(
         @NotBlank(message = "Title is required")
         String title,
--- event-hub/src/main/java/com/lucasmoraist/event_hub/domain/request/UserRequest.java
+++ event-hub/src/main/java/com/lucasmoraist/event_hub/domain/request/UserRequest.java
@@ -1,10 +1,23 @@
 package com.lucasmoraist.event_hub.domain.request;
 
 import com.lucasmoraist.event_hub.domain.enums.Roles;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(
+        description = "Request object for creating or updating a user",
+        requiredProperties = {"name", "email", "password", "roles"},
+        example = """
+                {
+                        "name": "John Doe",
+                        "email": "john@doe.com",
+                        "password": "securePassword123",
+                        "roles": "USER"
+                }
+                """
+)
 public record UserRequest(
         @NotBlank(message = "Name is required")
         String name,
