57f3fce1235626b2313d7e556e66732a430f68c6
lucasmoraist <luksmnt1101@gmail.com>
2025-06-09 14:31:19 -0300

    test: add unit test to events use cases

---
+++ event-hub/src/test/java/com/lucasmoraist/event_hub/application/usecases/events/CreateEventUseCaseTest.java
@@ -0,0 +1,77 @@
+package com.lucasmoraist.event_hub.application.usecases.events;
+
+import com.lucasmoraist.event_hub.domain.entity.Events;
+import com.lucasmoraist.event_hub.domain.exception.SameEventException;
+import com.lucasmoraist.event_hub.domain.request.EventsRequest;
+import com.lucasmoraist.event_hub.infra.repository.EventsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CreateEventUseCaseTest {
+
+    @Mock
+    EventsRepository repository;
+    @Mock
+    MongoTemplate mongoTemplate;
+    @InjectMocks
+    CreateEventUseCase createEventUseCase;
+
+    EventsRequest request;
+
+    @BeforeEach
+    void setUp() {
+        request = EventsRequest.builder()
+                .title("Tech Conference 2023")
+                .description("A conference about the latest in technology.")
+                .date(LocalDate.of(2023, 10, 15))
+                .time(LocalTime.of(9, 0))
+                .location("Convention Center, Cityville")
+                .capacity(500)
+                .createdBy("admin")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Should create an event successfully")
+    void case01() {
+        Events events = new Events(request);
+        when(repository.save(any(Events.class))).thenReturn(events);
+
+        createEventUseCase.execute(request);
+
+        assertNotNull(request);
+        assertNotNull(events);
+        verify(repository, times(1))
+                .save(any(Events.class));
+    }
+
+    @Test
+    @DisplayName("Should throw SameEventException when event with same name, date, time and location exists")
+    void case02() {
+        when(mongoTemplate.count(any(), any(Class.class))).thenReturn(1L);
+
+        assertThrows(SameEventException.class, () -> createEventUseCase.execute(request));
+
+        verify(repository, times(0))
+                .save(any(Events.class));
+        verify(mongoTemplate, times(1))
+                .count(any(), any(Class.class));
+    }
+
+}
---
+++ event-hub/src/test/java/com/lucasmoraist/event_hub/application/usecases/events/FindEventByIdTest.java
@@ -0,0 +1,92 @@
+package com.lucasmoraist.event_hub.application.usecases.events;
+
+import com.lucasmoraist.event_hub.domain.entity.Events;
+import com.lucasmoraist.event_hub.domain.exception.NotFoundException;
+import com.lucasmoraist.event_hub.domain.request.EventsRequest;
+import com.lucasmoraist.event_hub.domain.response.EventsResponse;
+import com.lucasmoraist.event_hub.infra.repository.EventsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FindEventByIdTest {
+
+    @Mock
+    EventsRepository repository;
+    @InjectMocks
+    FindEventById findEventById;
+
+    String eventId;
+    EventsRequest request;
+    Events events;
+    EventsResponse response;
+
+    @BeforeEach
+    void setUp() {
+        eventId = "12345";
+        request = EventsRequest.builder()
+                .title("Event Name")
+                .description("Event Description")
+                .date(LocalDate.of(2023, 10, 1))
+                .time(LocalTime.of(10, 0))
+                .location("Location")
+                .capacity(100)
+                .createdBy("Organizer Name")
+                .build();
+        events = new Events(request);
+        response = new EventsResponse(events);
+    }
+
+    @Test
+    @DisplayName("Should return event by id")
+    void case01() {
+        when(repository.findById(eventId)).thenReturn(Optional.of(events));
+
+        var find = findEventById.execute(eventId);
+
+        assertAll(
+                () -> assertNotNull(eventId),
+                () -> assertNotNull(find),
+                () -> assertEquals(response.id(), find.id()),
+                () -> assertEquals(response.title(), find.title()),
+                () -> assertEquals(response.description(), find.description()),
+                () -> assertEquals(response.date(), find.date()),
+                () -> assertEquals(response.time(), find.time()),
+                () -> assertEquals(response.location(), find.location()),
+                () -> assertEquals(response.capacity(), find.capacity()),
+                () -> assertEquals(response.createdBy(), find.createdBy()),
+                () -> verify(repository, times(1))
+                        .findById(eventId)
+        );
+    }
+
+    @Test
+    @DisplayName("Should throw NotFoundException when event not found")
+    void case02() {
+        when(repository.findById(eventId)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(NotFoundException.class, () -> findEventById.execute(eventId));
+
+        assertAll(
+                () -> assertNotNull(exception),
+                () -> assertEquals("Event not found", exception.getMessage()),
+                () -> verify(repository, times(1))
+                        .findById(eventId)
+        );
+    }
+
+}
