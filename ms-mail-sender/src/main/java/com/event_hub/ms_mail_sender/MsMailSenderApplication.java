538935259aa474ae593e10b520c4244295b455c3
lucasmoraist <luksmnt1101@gmail.com>
2025-06-10 09:35:35 -0300

    feat: initialize ms-mail-sender application with Gradle setup and basic configuration

---
+++ ms-mail-sender/src/main/java/com/event_hub/ms_mail_sender/MsMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.event_hub.ms_mail_sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMailSenderApplication.class, args);
+	}
+
+}
---
+++ ms-mail-sender/src/test/java/com/event_hub/ms_mail_sender/MsMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_hub.ms_mail_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
