db2688e28dceaa849aa8160c753fc06bad22e26a
lucasmoraist <luksmnt1101@gmail.com>
2025-06-11 19:49:53 -0300

    feat: add Feign clients for event and user service communication

---
+++ ms-inscriptions/src/main/java/com/event_hub/ms_inscriptions/infra/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.event_hub.ms_inscriptions.infra.client;
+
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-client", url = "${microservice.endpoint.user}")
+public interface UserClient {
+
+    @GetMapping("/{userId}")
+    Response getUserById(@PathVariable String userId);
+
+}
