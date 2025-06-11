ba87dc2e27dd66311311a98a6388ce738270a7e3
lucasmoraist <luksmnt1101@gmail.com>
2025-06-10 21:36:27 -0300

    feat: add custom exception classes for user management

---
+++ ms-user/src/main/java/com/event_hub/ms_user/domain/exception/PasswordException.java
@@ -0,0 +1,9 @@
+package com.event_hub.ms_user.domain.exception;
+
+public class PasswordException extends RuntimeException {
+
+    public PasswordException(String message) {
+        super(message);
+    }
+
+}
