87425780cc85072fc23eeee1f285290c3d29533a
lucasmoraist <luksmnt1101@gmail.com>
2025-06-10 22:07:28 -0300

    feat: initialize Spring Boot project with Gradle setup

---
+++ ms-events/src/test/java/com/event_hub/ms_events/MsEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_hub.ms_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
