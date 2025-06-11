e032856f171ed135f4f0e4d48d3311e4f319049d
lucasmoraist <luksmnt1101@gmail.com>
2025-06-10 22:04:11 -0300

    chore: remove unused exception

--- ms-user/src/main/java/com/event_hub/ms_user/domain/exception/InvalidFieldException.java
+++
@@ -1,9 +0,0 @@
-package com.event_hub.ms_user.domain.exception;
-
-public class InvalidFieldException extends RuntimeException {
-
-    public InvalidFieldException(String message) {
-        super(message);
-    }
-
-}
