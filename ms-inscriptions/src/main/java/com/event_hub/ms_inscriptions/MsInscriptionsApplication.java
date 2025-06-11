3081c75ea9366324809ff26a631f25204fda11ef
lucasmoraist <luksmnt1101@gmail.com>
2025-06-11 18:33:59 -0300

    feat: enable Feign clients for service communication

--- ms-inscriptions/src/main/java/com/event_hub/ms_inscriptions/MsInscriptionsApplication.java
+++ ms-inscriptions/src/main/java/com/event_hub/ms_inscriptions/MsInscriptionsApplication.java
@@ -2,8 +2,10 @@ package com.event_hub.ms_inscriptions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsInscriptionsApplication {
 
 	public static void main(String[] args) {
