5a13e65a699d3ef57d1688ded9ae51a57b6c50a8
lucasmoraist <luksmnt1101@gmail.com>
2025-06-26 17:14:59 -0300

    feat: add server structure

---
+++ server/src/test/java/com/event_hub/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_hub.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
